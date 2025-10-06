86683138f4006d619fc93a8e80528f578a0435bf
batu <batuhanozdic16@gmail.com>
2025-10-06 21:28:37 +0300

    Update * Save - PasswordManager.cs
    A Saving System

--- PasswordManager.cs
+++ PasswordManager.cs
@@ -1,15 +1,46 @@
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-
+using System.Drawing;
 class Program
 {
     static void Main()
     {
         List<string> Passwords = new List<string>();
+        string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "password.txt");
 
         while (true)
         {
-            Console.WriteLine("Welcome to password manager.");
+            
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("""
+                
+
+                
+
+                 ________  ________  _________  ___  ___  ________  ________    _____  ________      
+                |\   __  \|\   __  \|\___   ___\\  \|\  \|\   ____\|\   ____\  / __  \|\   ____\     
+                \ \  \|\ /\ \  \|\  \|___ \  \_\ \  \\\  \ \  \___|\ \  \___|_|\/_|\  \ \  \___|_    
+                 \ \   __  \ \   __  \   \ \  \ \ \  \\\  \ \  \    \ \_____  \|/ \ \  \ \_____  \   
+                  \ \  \|\  \ \  \ \  \   \ \  \ \ \  \\\  \ \  \____\|____|\  \   \ \  \|____|\  \  
+                   \ \_______\ \__\ \__\   \ \__\ \ \_______\ \_______\____\_\  \   \ \__\____\_\  \ 
+                    \|_______|\|__|\|__|    \|__|  \|_______|\|_______|\_________\   \|__|\_________\
+                                                                      \|_________|       \|_________|
+
+                
+
+
+
+
+
+
+
+
+
+                """);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Welcome to Password Manager.");
             Console.WriteLine("Type 1 to add a password");
             Console.WriteLine("Type 2 to view saved passwords.");
             Console.WriteLine("Type 3 to exit.");
@@ -27,6 +58,8 @@ class Program
                 string Purpose = Console.ReadLine();
                 Passwords.Add(PassAdder + "-" + Purpose);
                 Console.Clear();
+                string lineToSave = $"Password: {PassAdder} - Purpose: {Purpose}";
+                File.AppendAllText(filePath, lineToSave + Environment.NewLine);
                 Console.WriteLine("Added to your saved passwords.");
             }
             else if (choice == '2')
@@ -38,12 +71,19 @@ class Program
                 }
                 else
                 {
-                    Console.WriteLine("Your passwords:");
-                    foreach (string Password in Passwords)
+                    Console.Clear();
+                    string[] allPasswds = File.ReadAllLines(filePath);
+                    foreach (string passwordLine in allPasswds)
                     {
-                        Console.WriteLine(Password);
+                        Console.WriteLine(passwordLine);
                     }
-                    Console.WriteLine();
+                    Console.ForegroundColor= ConsoleColor.DarkRed;
+                    Console.WriteLine("It Will Be Updated Soon");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine("\nPress any key to return to the menu...");
+                    Console.ReadKey();
+                    Console.Clear();
                 }
             }
             else if (choice == '3')
